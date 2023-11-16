dedfbc04e72e33a05d3bf02ffd1776d16a66ef89
Paxton <paxton@drab.org>
2023-11-15 18:10:13 -0600

    program M08

--- M08.py
+++ M08.py
@@ -6,13 +6,10 @@ from pybricks.parameters import Port, Stop, Direction, Button, Color
 from pybricks.tools import wait, StopWatch, DataLog
 from pybricks.robotics import DriveBase
 from pybricks.media.ev3dev import SoundFile, ImageFile
-from pybricks.nxtdevices import LightSensor
 
-from robot import Generic_Robot
 from robot import Robot_Plus
-from M01 import mo1
-from M08 import mo8
 
 robot = Robot_Plus()
 
-mo8(robot)
+def mo8(robot):
+  robot.gyro_drive(0, 150, 440)
