a63de5fa19ed34b1620123b7da2c0fec2eb08495
Paxton <paxton@drab.org>
2023-11-20 19:24:12 -0600

    test M09

--- main.py
+++ main.py
@@ -10,10 +10,13 @@ from pybricks.nxtdevices import LightSensor
 
 from robot import Generic_Robot
 from robot import Robot_Plus
+from scrim_master import start
+
 from M09 import mo9
 from M08 import mo8
 from M04 import mo4a, mo4b
 
 robot = Robot_Plus()
 
-mo8(robot)
+
+mo9(robot)
