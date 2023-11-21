9adf298ddf9eb90eddb559c177fe1f9123b13184
Paxton <paxton@drab.org>
2023-11-20 19:24:20 -0600

    edit scrim code

--- scrim_master.py
+++ scrim_master.py
@@ -1,4 +1,11 @@
-
+#!/usr/bin/env pybricks-micropython
+from pybricks.hubs import EV3Brick
+from pybricks.ev3devices import (Motor, TouchSensor, ColorSensor,
+                                 InfraredSensor, UltrasonicSensor, GyroSensor)
+from pybricks.parameters import Port, Stop, Direction, Button, Color
+from pybricks.tools import wait, StopWatch, DataLog
+from pybricks.robotics import DriveBase
+from pybricks.media.ev3dev import SoundFile, ImageFile
 from robot import Robot_Plus
 
 from M09 import mo9
@@ -6,31 +13,33 @@ from M04 import mo4b
 from M08 import mo8
 
 robot = Robot_Plus()
+img = ImageFile()
 button_press = robot.ev3.buttons.pressed()
 
-while True:
-  # M09 #
-  while button_press != [CENTER]:
-    # check for advancing to next
-    if button_press == [DOWN]:
-      break
-  mo9(robot)
-  break
+def start(robot):
+  while True:
+    # M09 #
+    while button_press != [Button.CENTER]:
+      # check for advancing to next
+      if button_press == [Button.DOWN]:
+        break
+    mo9(robot)
+    break
 
-while True:
-  # M04 #
-  while button_press != [CENTER]:
-    # check for advancing to next
-    if button_press == [DOWN]:
-      break
-  mo4b(robot)
-  break
+  while True:
+    # M04 #
+    while button_press != [Button.CENTER]:
+      # check for advancing to next
+      if button_press == [Button.DOWN]:
+        break
+    mo4b(robot)
+    break
 
-while True:
-  # M08 #
-  while button_press != [CENTER]:
-    # check for advancing to next
-    if button_press == [DOWN]:
-      break
-  mo8(robot)
-  break
+  while True:
+    # M08 #
+    while button_press != [Button.CENTER]:
+      # check for advancing to next
+      if button_press == [Button.DOWN]:
+        break
+    mo8(robot)
+    break
