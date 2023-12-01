5653c5186f4eb3f04034cb314e9a75d228034c2b
Paxton <paxton@drab.org>
2023-12-01 17:06:25 -0600

    fix M02

--- M02.py
+++ M02.py
@@ -2,7 +2,7 @@ def mo2():
   commands = [
     ['gyro_drive',[0, 150, 100, 'True']],
     ['pivot',[30, 100]],
-    ['gyro_drive',[0, 150, 100, 'True']],
+    ['gyro_drive',[0, 150, 550, 'True']],
     ['pivot',[-90, 100]],
     ['gyro_drive',[0, 150, 100, 'True']],
     ['drive_mm',[0, 150, -50]],
