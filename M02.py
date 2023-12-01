e866a6108fbe22348fa85a3970bd84a9e8dcfe6e
Paxton <paxton@drab.org>
2023-12-01 13:22:01 -0600

    implement M02 with abort()

--- M02.py
+++ M02.py
@@ -13,19 +13,20 @@ from robot import Robot_Plus
 robot = Robot_Plus()
 
 def mo2(robot):
-  robot.gyro_drive(0, 150, 100)
-  robot.pivot(30, 100)
-  robot.gyro_drive(0, 150, 550)
-  robot.pivot(-90, 100)
+  commands = [
+    ['gyro_drive',[0, 150, 100]],
+    ['pivot',[30, 100]],
+    ['gyro_drive',[0, 150, 100]],
+    ['pivot',[-90, 100]],
+    ['gyro_drive',[0, 150, 100]],
+    ['drive_mm'],[0, 150, -50],
 
-  robot.gyro_drive(0, 150, 100)
-  robot.drive_mm(0, 150, -50)
+    #this is for pink add this code to add more pushes
 
+    #robot.gyro_drive(0, 150, 50)
+    # robot.drive_mm(0, 150, -50)
 
-  #this is for pink add this code to add more pushes
-
-  #robot.gyro_drive(0, 150, 50)
-  # robot.drive_mm(0, 150, -50)
-
-  robot.pivot(60, 100)
-  robot.drive_mm(0, 500, -800)
+    ['pivot',[60, 100]],
+    ['drive_mm',[0, 500, -800]]
+  ]
+  return commands
