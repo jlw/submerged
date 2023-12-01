f0451ba40b44f00ad2cd5b16f1e4cb2c132002c4
Paxton <paxton@drab.org>
2023-12-01 15:14:54 -0600

    fix M09

--- M09.py
+++ M09.py
@@ -1,14 +1,14 @@
 def mo9():
   commands = [
     ['gyro_drive',[0, 150, 455]],
-    ['act_right.run_time',[-1000, 600, 'wait=False']],
+    ['act_run_time',[-1000, 600, 'right', 'False']],
     ['wait',[350]],
     ['pivot',[-10, 100]],
     ['drive_mm',[0, 150, -100]],
     ['pivot',[-55, 150]],
-    ['act_right.run_angle',[120, 100]],
+    ['act_run_angle',[120, 100, 'right', 'True']],
     ['pivot',[45, 100]],
     ['drive_mm',[0, 200, -300]],
-    ['act_right.run_time',[1000, 500]]
+    ['act_run_time',[1000, 500, 'right', 'True']]
   ]
   return commands
