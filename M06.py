3a40603b053035ce69485ce2af8177e72c4ecd17
Paxton <paxton@drab.org>
2023-12-01 18:06:47 -0600

    implement M06 with abort()

--- M06.py
+++ M06.py
@@ -1,43 +1,33 @@
-#!/usr/bin/env pybricks-micropython
-from pybricks.hubs import EV3Brick
-from pybricks.ev3devices import (Motor, TouchSensor, ColorSensor,
-                                 InfraredSensor, UltrasonicSensor, GyroSensor)
-from pybricks.parameters import Port, Stop, Direction, Button, Color
-from pybricks.tools import wait, StopWatch, DataLog
-from pybricks.robotics import DriveBase
-from pybricks.media.ev3dev import SoundFile, ImageFile
-
-from robot import Robot_Plus
-
-robot = Robot_Plus()
-
-def mo6(robot):
-  robot.drive_mm(0, 200, 100)
-  wait(200)
-  robot.pivot(35, 100)
-  robot.gyro_drive(0, 200, 630)
-  wait(200)
-  robot.pivot(55, 100)
-  robot.gyro_drive(0, 200, 400)
-  robot.pivot(-60, 100)
-  robot.drive_mm(0, 150, 200)
-  robot.pivot(45, 100)
-  robot.act_left.run_time(-700, 1000, wait=False)
-  robot.act_right.run_time(700, 400)
-  robot.drive_mm(0, 75, 30)
-  robot.pivot(30, 250)
-  robot.act_right.run_time(-700, 250, wait=False)
-  robot.gyro_drive(0, 150, 200)
-  robot.pivot(-45, 100)
-  wait(150)
-  robot.gyro_drive(0, 150, 400)
-  robot.pivot(-60, 100)
-  wait(150)
-  robot.act_right.run_time(-200, 1000, wait=False)
-  robot.drive_mm(0, 75, 140)
-  robot.act_right.run_time(700, 400)
-  wait(100)
-  robot.pivot(20, 100)
-  robot.drive_mm(0, 200, -100)
-  robot.pivot(-90, 100)
-  robot.drive_mm(0, 300, -750)
+def mo6():
+  commands = [
+    ['drive_mm',[0, 200, 100]],
+    ['wait',[200]],
+    ['pivot',[35, 100]],
+    ['gyro_drive',[0, 200, 630]],
+    ['wait',[200]],
+    ['pivot',[55, 100]],
+    ['gyro_drive',[0, 200, 400]],
+    ['pivot',[-60, 100]],
+    ['dirve_mm',[0, 150, 200]],
+    ['pivot',[45, 100]],
+    ['act_run_time',[-700, 1000, 'left', 'False']],
+    ['act_run_time',[700, 400, 'right', 'True']],
+    ['drive_mm',[0 75, 30]],
+    ['pivot',[30, 250]],
+    ['act_run_time',[-700, 250, 'right', 'False']],
+    ['gyro_drive',[0, 150, 200]],
+    ['pivot',[-45, 100]],
+    ['wait',[150]],
+    ['gyro_drive',[0, 150, 400]],
+    ['pivot',[-60, 100]],
+    ['wait',[150]],
+    ['act_run_time',[-200, 1000, 'right', 'False']],
+    ['drive_mm',[0, 75, 140]],
+    ['act_run_time',[700, 400, 'right', 'True']],
+    ['wait',[100]],
+    ['pivot',[20, 100]],
+    ['drive_mm',[0, 200, -100]],
+    ['pivot',[-90, 100]],
+    ['drive_mm',[0, 300, -750]]
+  ]
+  return commands
