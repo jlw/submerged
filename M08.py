221f691a07aa19723041f324c49eeae82798f94d
Paxton <paxton@drab.org>
2024-01-01 16:06:20 -0600

    add command mod to M08

--- M08.py
+++ M08.py
@@ -1,19 +1,6 @@
 import Commands
 
 def mo8():
-  commands = [
-    ['gyro_drive',[0, 150, 60, 'True']],
-    ['pivot',[-100, 100]],
-    ['gyro_drive',[0, 150, 300, 'True']],
-    ['pivot',[45, 100]],
-    ['gyro_drive',[0, 200, 350, 'True']],
-    ['high_tork_time',[400, 4750]],
-    ['drive_mm',[0, 150, -300]],
-    ['pivot',[-55, 100]],
-    ['drive_mm',[0, 200, 190]],
-    ['drive_mm',[0, 250, -380]]
-  ]
-
   return [
     Commands.GyroDrive(speed=150, distance=60),
     Commands.Pivot(angle=-100, speed=100),
