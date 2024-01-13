3b6777651de478a54837adc6a0a6e67b082528f5
Paxton <paxton@drab.org>
2024-01-13 16:52:38 -0600

    add M09

--- M09.py
+++ M09.py
@@ -1,16 +1,15 @@
+import Commands
+
 def mo9():
-  commands = [
-    ['gyro_drive',[0, 150, 440, 'True']],
-    ['act_run_time',[-1000, 750, 'right', 'False']],
-    ['wait',[350]],
-    ['pivot',[-10, 100]],
-    ['drive_mm',[0, 150, -100]],
-    ['pivot',[-55, 150]],
-    ['act_run_time',[300, 1000, 'right', 'True']],
-    ['pivot',[20, 100]],
-    ['act_run_time',[-700, 500, 'right', 'True']],
-    ['pivot',[25, 100]],
-    ['drive_mm',[0, 200, -300]],
-    ['act_run_time',[1000, 1000, 'right', 'True']],
+  return [
+    Commands.GyroDrive(speed=150, distance=390),
+    Commands.ActMotorTime(motor="left", speed=-400, time=500, wait=False),
+    Commands.ActMotorTime(motor="right", speed=300, time=3000),
+    Commands.Wait(time=300),
+    Commands.ActMotorTime(motor="right", speed=-700, time=2000, wait=False),
+    Commands.DriveMM(speed=150, distance=-100),
+    Commands.Pivot(angle=-45, speed=100),
+    Commands.ActMotorTime(motor="left", speed=500, time=250),
+    Commands.Pivot(angle=45, speed=250),
+    Commands.DriveMM(speed=250, distance=-300)
   ]
-  return commands
