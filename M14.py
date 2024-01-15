1bce97d7136f8725ea21a81f78c90c9d82f61e36
Paxton <paxton@drab.org>
2024-01-15 13:33:36 -0600

    test w/ linesquare

--- M14.py
+++ M14.py
@@ -1,7 +1,11 @@
+import Commands
+
 def m14():
   commands = [
     ['gyro_drive',[0, 150, 100, 'True']],
     ['pivot',[-110, 100]],
     ['gyro_drive',[0, 150, 750, 'True']]
   ]
-  return commands
+  return [
+    Commands.LineSquare(target=20, targetBlack=13, targetWhite=80, approachSpeed=150, finetuneSpeed=50)
+  ]
