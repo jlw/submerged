c41028630ee1deb06ead0fea35d2767a01b8f295
Paxton <paxton@drab.org>
2024-01-16 15:41:05 -0600

    finish M08

--- M08.py
+++ M08.py
@@ -3,17 +3,14 @@ import Commands
 def mo8():
   return [
     Commands.Pivot(angle=-90, speed=100),
-    Commands.Wait(time=250),
     Commands.GyroDrive(speed=200, distance=165),
-    Commands.Wait(time=250),
-    Commands.Pivot(angle=45, speed=100),
-    Commands.Wait(time=250),
-    Commands.GyroDrive(speed=250, distance=350),
-    Commands.ActMotorTime(motor='left', speed=-1000, time=800),
-    Commands.GyroDrive(speed=250, distance=90),
-    Commands.ActMotorTime(motor='right', time=7500, speed=-1000),
+    Commands.Pivot(angle=46, speed=100),
+    Commands.GyroDrive(speed=250, distance=275),
+    Commands.ActMotorTime(motor='left', speed=1000, time=400),
+    Commands.GyroDrive(speed=250, distance=75),
+    Commands.ActMotorTime(motor='right', time=7000, speed=-1000),
     Commands.DriveMM(angle=0, speed=120, distance=-300),
-    Commands.Pivot(angle=-52, speed=100),
+    Commands.Pivot(angle=-50, speed=100),
     Commands.DriveMM(angle=0, speed=200, distance=190),
-    Commands.DriveMM(angle=0, speed=250, distance=-380)
+    Commands.DriveMM(angle=0, speed=250, distance=-500)
 ]
