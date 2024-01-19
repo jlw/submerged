1452f3478729fbe02f0b7f49701b1c3bfdd9f26c
Paxton <paxton@drab.org>
2024-01-19 11:30:15 -0600

    finish M02

--- M02.py
+++ M02.py
@@ -1,16 +1,6 @@
 import Commands
 
 def mo2():
-  commands = [
-    ['gyro_drive',[0, 150, 100, 'True']],
-    ['pivot',[30, 100]],
-    ['gyro_drive',[0, 150, 550, 'True']],
-    ['pivot',[-90, 100]],
-    ['gyro_drive',[0, 150, 100, 'True']],
-    ['drive_mm',[0, 150, -50]],
-    ['pivot',[60, 100]],
-    ['drive_mm',[0, 500, -800]]
-  ]
   return [
     # Drive towards M01 & lower left arm
     Commands.ActMotorTime(motor="left", speed=400, time=300, wait=False),
@@ -28,9 +18,9 @@ def mo2():
     Commands.ActMotorTime(motor="right", speed=-700, time=500, wait=False),
     # Push M02
     Commands.DriveMM(speed=75, distance=70),
-    Commands.ActMotorTime(speed=-400, motor="left", time=1700, wait=False),
+    Commands.ActMotorTime(speed=-400, motor="left", time=2000, wait=False),
     # Return to home
-    Commands.DriveMM(speed=75, distance=-100),
-    Commands.Pivot(speed=100, angle=60),
-    Commands.DriveMM(speed=200, distance=-600)
+    Commands.DriveMM(speed=75, distance=-80),
+    Commands.Pivot(speed=100, angle=75),
+    Commands.DriveMM(speed=200, distance=-700)
   ]
