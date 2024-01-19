5a0c8e99406de48bc66f8fa399e637468f06eb27
Paxton <paxton@drab.org>
2024-01-19 14:08:48 -0600

    finish M14

--- M14.py
+++ M14.py
@@ -9,7 +9,7 @@ def m14():
     Commands.ActMotorTime(motor='left', speed=-250, time=1000, wait=False),
     Commands.GyroDrive(angle=0, speed=150, distance=260, reset_sensor=False),
     Commands.ActMotorTime(motor='right', speed=-250, time=1000),
-    Commands.GyroDrive(speed=100, distance=240),
     Commands.ActMotorTime(motor='left', speed=250, time=1000),
+    Commands.GyroDrive(speed=100, distance=240),
     Commands.GyroDrive(speed=200, distance=800),
   ]
