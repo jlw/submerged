66580ef2a76eb56f0024015bf0c1d41f64d14319
Paxton <paxton@drab.org>
2024-03-09 16:48:10 -0600

    slow loops times for better response

--- pid.py
+++ pid.py
@@ -1,3 +1,5 @@
+from pybricks.tools import wait, StopWatch
+
 class PIDController:
   def __init__(self, gainP, gainI, gainD):
     self.gainP = gainP
@@ -6,8 +8,11 @@ class PIDController:
     self.integral = 0
     self.last_error = 0
 
+    self.desiredTime = 50
+    
   def adjust(self, error):
+    wait(self.desiredTime)
+
     self.integral = error + self.integral * (2/3)
     derivative = error - self.last_error
-    #print(error, self.last_error, derivative, self.integral, self.gainP, self.gainI, self.gainD)
     return (error * self.gainP) + (self.integral * self.gainI) + (derivative * self.gainD)
