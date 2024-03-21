2593664bd527cb2f605388dd4e69b4916e38ef0f
Paxton <paxton@drab.org>
2024-03-21 14:33:06 -0500

    add dirve_mtorto_ganel()

--- robot.py
+++ robot.py
@@ -216,7 +216,7 @@ class Robot_Plus(Generic_Robot):
     elif motor == "right": # <- Right Motor
       self.act_right.run_angle(speed, angle, wait=wait)
 
-  def drive_motor_angle(motor, speed, angle, wait):
+  def drive_motor_angle(self, motor, speed, angle, wait):
     if motor == "left":
       self.left_motor.run_angle(speed, angle, wait=wait)
     elif motor == "right":
