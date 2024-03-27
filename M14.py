eb6bedfe5457fc91a30c22a31898a95c8f79cbdc
Corwin Dennis <corwon52@gmail.com>
2024-03-27 15:50:16 -0500

    M14 v1 complete

--- M14.py
+++ M14.py
@@ -2,14 +2,18 @@ import Commands
 
 def m14():
   return [
-    Commands.DriveMM(angle=12, speed=125, distance=355),
-    Commands.Wait(time=75),
-    Commands.DriveMM(angle=-14, speed=125, distance=270),
-    Commands.Wait(time=75),
-    Commands.ActMotorTime(motor='left', speed=-250, time=1000, wait=False),
-    Commands.GyroDrive(angle=0, speed=150, distance=260, reset_sensor=False),
-    Commands.ActMotorTime(motor='right', speed=-250, time=1000),
-    Commands.ActMotorTime(motor='left', speed=250, time=1000),
-    Commands.GyroDrive(speed=100, distance=240),
-    Commands.GyroDrive(speed=200, distance=800),
+    Commands.DriveMM(speed=150, distance=185),
+    Commands.Pivot(angle=-90, speed=100),
+    Commands.GyroDrive(speed=175, distance=600),
+    Commands.ActMotorTime(motor='left', speed=100, time=1000),
+    Commands.DriveMM(speed=175, distance=190),
+    Commands.ActMotorTime(motor='right', speed=-100, time=1000),
+    Commands.DriveMM(speed=175, distance=185),
+    Commands.DriveMM(speed=175, distance=-50),
+    Commands.ActMotorTime(motor='left', speed=-100, time=1000),
+    Commands.GyroDrive(speed=175, distance=400),
+    Commands.ActMotorTime(motor='right', speed=100, time=1500, wait=False),
+    Commands.ActMotorTime(motor='left', speed=-100, time=1500, wait=False),
+    Commands.DriveMM(speed=200, distance=125),
+    Commands.GyroDrive(angle=-15, speed=250, distance=300),
   ]
