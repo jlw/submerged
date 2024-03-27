d826636c1a4a549eeb14f5cdfc9c4004f8ba058b
Corwin Dennis <corwon52@gmail.com>
2024-03-27 16:47:34 -0500

    M08 progress,

--- M08.py
+++ M08.py
@@ -15,10 +15,14 @@ def mo8():
 #     Commands.DriveMM(angle=0, speed=250, distance=-500)
 
 
-  Commands.GyroDrive(speed=150, distance=3),
-  Commands.Pivot(angle=-90, speed=100),
-  Commands.GyroDrive(speed=200, distance=311.5),
-  Commands.GyroDrive(speed=200, distance=-200),
-  Commands.Pivot(angle=45, speed=150)
-  #Commands.GyroDrive()
+  Commands.GyroDrive(speed=150, distance=25),
+  Commands.Pivot(angle=-94, speed=80),
+  Commands.GyroDrive(speed=200, distance=360),
+  Commands.DriveMM(speed=200, distance=-170),
+  Commands.Pivot(angle=50, speed=150),
+  Commands.ActMotorTime(motor='right', speed=-999, time=500, wait=False),
+  Commands.GyroDrive(speed=200, distance=333),
+  Commands.Wait(Time=1000),
+  Commands.ActMotorTime(motor='right', speed=999, time=100, wait=False),
+  Commands.ActMotorTime(motor='left', speed=-999, time=4000),
 ]
