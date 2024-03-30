2ddffdd999dd2854d9bc6516b41215a272886e38
Corwin Dennis <corwon52@gmail.com>
2024-03-30 12:10:48 -0500

    M08 full cinda works

--- M08.py
+++ M08.py
@@ -15,14 +15,17 @@ def mo8():
 #     Commands.DriveMM(angle=0, speed=250, distance=-500)
 
 
+  Commands.ActMotorTime(motor='right', speed=-999, time=600, wait=False),
   Commands.GyroDrive(speed=150, distance=25),
-  Commands.Pivot(angle=-94, speed=80),
+  Commands.Pivot(angle=-95, speed=80),
+  Commands.ActMotorTime(motor='right', speed=999, time=455, wait=False),
   Commands.GyroDrive(speed=200, distance=360),
-  Commands.DriveMM(speed=200, distance=-170),
+  Commands.DriveMM(speed=200, distance=-190),
   Commands.Pivot(angle=50, speed=150),
-  Commands.ActMotorTime(motor='right', speed=-999, time=500, wait=False),
-  Commands.GyroDrive(speed=200, distance=333),
-  Commands.Wait(Time=1000),
-  Commands.ActMotorTime(motor='right', speed=999, time=100, wait=False),
-  Commands.ActMotorTime(motor='left', speed=-999, time=4000),
+  Commands.GyroDrive(speed=200, distance=345),
+  Commands.ActMotorTime(motor='right', speed=-999, time=750, wait=False),
+  Commands.ActMotorTime(motor='left', speed=-99999, time=1250),
+  Commands.DriveMM(speed=250, distance=-200),
+  Commands.Pivot(speed=250, angle=-25),
+  Commands.DriveMM(speed=250, distance=-400),
 ]
