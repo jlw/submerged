715b3213d0ddb3622ff20e7677d9c2d3ea7684b4
Zane Ellingson <zane@34334.net>
2024-04-01 20:11:08 -0500

    the last line might not be accurate

--- M10.py
+++ M10.py
@@ -12,8 +12,13 @@ def m10():
     # Commands.ActMotorTime(motor="left", speed=500, time=250),
     # Commands.Pivot(angle=45, speed=250),
     # Commands.DriveMM(speed=250, distance=-300)--backwards
+  Commands.ActMotorTime(motor="left", speed=-1000, time=900, wait=False),
   Commands.GyroDrive(speed=150, distance=15),
-  Commands.Pivot(angle=45, speed=150),
+  Commands.Pivot(angle=42, speed=150),
   Commands.GyroDrive(speed=175, distance=600),
-  Commands.DriveMM(speed=200,distance=-400)
+  Commands.DriveMM(speed=200,distance=-400),
+  Commands.Pivot(angle=48, speed=150),
+  Commands.GyroDrive(speed=200, distance=420),
+  Commands.ActMotorTime(motor="right", speed=400, time=900, wait=False),
+  Commands.ActMotorTime(motor="left", speed=500, time=900, wait=True),
   ]
