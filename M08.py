09a2850f70fc69205474cc2e7a28a4284430499c
Corwin Dennis <corwon52@gmail.com>
2024-04-01 20:08:43 -0500

    M08 slight changes corwin and willam

--- M08.py
+++ M08.py
@@ -3,9 +3,8 @@ import Commands
 def mo8():
   return [
   # Reset Arm for Izzy
-  Commands.ActMotorTime(motor='right', speed=-999, time=600, wait=False),
   # Drive Forward and Pivot to face M08
-  Commands.GyroDrive(speed=150, distance=25),
+  Commands.DriveMM(speed=150, distance=25),
   Commands.Pivot(angle=-95, speed=80),
   # Lower arm
   Commands.ActMotorTime(motor='right', speed=999, time=455, wait=False),
@@ -17,7 +16,7 @@ def mo8():
   # *BRZZZT*  Mayday! Mayday! Chicken ahead! AAAAHHHHHH! *BRRZzzt*
   Commands.GyroDrive(speed=200, distance=345),
   # Yoink Izzy, Spin pablo
-  Commands.ActMotorTime(motor='right', speed=-999, time=750, wait=False),
+  Commands.ActMotorTime(motor='right', speed=-250, time=1750, wait=False),
   Commands.ActMotorTime(motor='left', speed=-99999, time=1250),
   # Escape the crime scene
   Commands.DriveMM(speed=250, distance=-200),
