91ef40ee90c0be42b484a716e7ddc26d996c8a8f
Corwin Dennis <corwon52@gmail.com>
2024-04-02 20:57:37 -0500

    almoast done corwin

--- M02.py
+++ M02.py
@@ -8,7 +8,7 @@ def mo2():
     Commands.GyroDrive(speed=200, distance=280),
     Commands.DriveMotor(motor="right", angle=250, speed=150),
     # Drop arm for Sam
-    Commands.ActMotorTime(motor="left", time=850, speed=-300, wait=False),
+    Commands.ActMotorTime(motor="left", time=600, speed=-300, wait=False),
     # Drive to M02 & push
     Commands.GyroDrive(speed=200, distance=160),
     Commands.DriveMotor(motor="right", angle=200, speed=150),
@@ -17,9 +17,11 @@ def mo2():
     Commands.ActMotorTime(motor="left", time=800, speed=350, wait=False),
     Commands.DriveMM(speed=100, distance=-80),
     # Deliver Izzy
-    Commands.ActMotorAngle(motor="right", speed=400, angle=370),
-    Commands.Wait(time=600),
-    Commands.ActMotorAngle(motor="right", speed=400, angle=-370),
+    Commands.ActMotorTime(motor="right", speed=600, time=800),
+    Commands.ActMotorTime(motor="right", speed=-999, time=100),
+    Commands.ActMotorTime(motor="right", speed=999, time=100),
+    Commands.ActMotorTime(motor="right", speed=-800, time=800),
+
     # Pivot towards M01
     Commands.Pivot(angle=-90, speed=100),
     # Manuver around M10
@@ -30,16 +32,17 @@ def mo2():
     Commands.DriveMotor(speed=-100, angle=50, motor="left"),
     # Complete M03 & M11
     Commands.ActMotorTime(motor="right", speed=700, time=1500),
-    Commands.ActMotorTime(motor="right", speed=-600, time=900, wait=False),
+    Commands.ActMotorTime(motor="right", speed=-800, time=1200, wait=False),
+    Commands.Wait(time=1000),
     # Pivot & Drive between M10 & M11
     Commands.Pivot(angle=45, speed=100),
     Commands.DriveMM(speed=250, distance=-100),
-    Commands.ActMotorTime(motor="left", speed=500, time=3500),
+    Commands.ActMotorTime(motor="left", speed=500, time=5000),
     # Face & Return to home
     Commands.DriveMM(speed=200, distance=50),
-    Commands.Pivot(angle=-90, speed=100),
+    Commands.Pivot(angle=-100, speed=100),
     Commands.DriveMM(speed=250, distance=600),
-    Commands.Pivot(angle=60, speed=100),
+    Commands.Pivot(angle=50, speed=100),
     Commands.DriveMM(speed=250, distance=500),
 
   ]
