c7bedf95d6539fb30e4c8703bff6cd0c7ef9232c
Paxton <paxton@drab.org>
2024-04-06 14:00:55 -0500

    make M10 go home (WILLIAM WUZ HER)

--- M10.py
+++ M10.py
@@ -2,24 +2,26 @@ import Commands
 
 def m10():
   return [
-    # Commands.GyroDrive(speed=150, distance=390),
-    # Commands.ActMotorTime(motor="left", speed=-400, time=500, wait=False),
-    # Commands.ActMotorTime(motor="right", speed=300, time=3000),
-    # Commands.Wait(time=300),
-    # Commands.ActMotorTime(motor="right", speed=-700, time=2000, wait=False),
-    # Commands.DriveMM(speed=150, distance=-100),
-    # Commands.Pivot(angle=-45, speed=100),
-    # Commands.ActMotorTime(motor="left", speed=500, time=250),
-    # Commands.Pivot(angle=45, speed=250),
-    # Commands.DriveMM(speed=250, distance=-300)--backwards
-  Commands.ActMotorTime(motor="left", speed=-1000, time=900, wait=False),
+  # Drive to M10
   Commands.GyroDrive(speed=150, distance=15),
   Commands.Pivot(angle=42, speed=150),
+  # Push M10
   Commands.GyroDrive(speed=175, distance=600),
+  # Back up & align with M09
   Commands.DriveMM(speed=200,distance=-400),
   Commands.Pivot(angle=48, speed=150),
+  # Drive to M09 camera
   Commands.GyroDrive(speed=200, distance=420),
+  # Capture Camera & Deliver Sam
   Commands.ActMotorTime(motor="right", speed=400, time=900, wait=False),
   Commands.ActMotorTime(motor="left", speed=500, time=900, wait=True),
-  Commands.GyroDrive(speed=-150, distance=150)
+  # Back up & move camera to zone
+  Commands.DriveMM(speed=200, distance=-150),
+  Commands.Pivot(speed=150, angle=-45),
+  # Deposit camera
+  Commands.ActMotorTime(motor="left", speed=-500, time=800),
+  # Leave
+  Commands.DriveMM(speed=200, distance=-100),
+  Commands.Pivot(angle=45, speed=500),
+  Commands.DriveMM(distance=-250, speed=500)
   ]
