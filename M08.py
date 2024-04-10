17167bd188b51fa29add0ca52c9df69306516794
Corwin Dennis <corwon52@gmail.com>
2024-04-10 16:28:27 -0500

    M08 finalised

--- M08.py
+++ M08.py
@@ -2,22 +2,29 @@ import Commands
 
 def mo8():
   return [
-  # Reset Arm for Izzy
-  # Drive Forward and Pivot to face M08
+  # Inch forward
   Commands.DriveMM(speed=150, distance=25),
-  Commands.Pivot(angle=-93, speed=80),
-  # Drive to & Push M08
-  Commands.GyroDrive(speed=200, distance=340),
-  # Back up & aim for pablo
+  #Pivot to face camera
+  Commands.Pivot(angle=-92, speed=80),
+  # Drive & stab the rollar coaster
+  Commands.GyroDrive(speed=200, distance=300),
+  # Retreat
   Commands.DriveMM(speed=200, distance=-190),
+  # Aim for pablo
   Commands.Pivot(angle=50, speed=150),
-  # *BRZZZT*  Mayday! Mayday! Chicken ahead! AAAAHHHHHH! *BRRZzzt*
-  Commands.GyroDrive(speed=200, distance=345),
-  # Yoink Izzy, Spin pablo
-  Commands.ActMotorTime(motor='right', speed=-250, time=1750, wait=False),
-  Commands.ActMotorTime(motor='left', speed=-99999, time=1250),
+  # Target accquired, push forward
+  Commands.GyroDrive(speed=200, distance=350),
+  # Impale Izzy
+  Commands.ActMotorTime(motor='right', speed=-250, time=600),
+  # Align for overspun poultry
+  Commands.DriveMM(speed=150, distance=20),
+  Commands.ActMotorTime(motor='left', speed=200, time=250),
+  # Progressively make pablo more dizzy
+  Commands.ActMotorTime(motor='left', speed=-999, time=3500),
   # Escape the crime scene
   Commands.DriveMM(speed=250, distance=-200),
-  Commands.Pivot(speed=250, angle=-25),
-  Commands.DriveMM(speed=250, distance=-400),
+  # Change course for safe house
+  Commands.Pivot(speed=250, angle=-30),
+  # Bunker down
+  Commands.DriveMM(speed=250, distance=-500),
 ]
