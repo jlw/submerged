6a76589156abadb6cc8ca99f30890ca0bd964b7d
Zane Ellingson <zane@34334.net>
2024-04-13 17:39:44 -0500

    test M14

--- M14.py
+++ M14.py
@@ -5,9 +5,13 @@ def m14():
     # Move off wall
     Commands.DriveMM(speed=150, distance=185),
     # Align with M08
-    Commands.Pivot(angle=-90, speed=100),
+    Commands.Pivot(angle=-95, speed=100),
+    Commands.DriveMM(speed=200, distance=300),
+    Commands.Pivot(angle=15, speed=150),
+    Commands.DriveMM(speed=150, distance=50),
+    Commands.Pivot(angle=-7, speed=150),
     # Drive and drop arm
-    Commands.GyroDrive(speed=175, distance=600),
+    Commands.GyroDrive(speed=175, distance=300),
     Commands.ActMotorTime(motor='left', speed=100, time=1000),
     # Release audience member
     Commands.DriveMM(speed=175, distance=190),
