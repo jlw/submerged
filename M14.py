64e80048e930e0636582c787141681de9ed144dd
Paxton <paxton@drab.org>
2024-04-14 16:33:10 -0500

    Revert "test M14"
    This reverts commit 6a76589156abadb6cc8ca99f30890ca0bd964b7d.

--- M14.py
+++ M14.py
@@ -5,13 +5,9 @@ def m14():
     # Move off wall
     Commands.DriveMM(speed=150, distance=185),
     # Align with M08
-    Commands.Pivot(angle=-95, speed=100),
-    Commands.DriveMM(speed=200, distance=300),
-    Commands.Pivot(angle=15, speed=150),
-    Commands.DriveMM(speed=150, distance=50),
-    Commands.Pivot(angle=-7, speed=150),
+    Commands.Pivot(angle=-90, speed=100),
     # Drive and drop arm
-    Commands.GyroDrive(speed=175, distance=300),
+    Commands.GyroDrive(speed=175, distance=600),
     Commands.ActMotorTime(motor='left', speed=100, time=1000),
     # Release audience member
     Commands.DriveMM(speed=175, distance=190),
