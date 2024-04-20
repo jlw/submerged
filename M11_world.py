f612776b48330ee978821d39a3fadf4cdac69ed9
Corwin Dennis <corwon52@gmail.com>
2024-04-20 10:49:48 -0500

    m11 ally changes

--- M11_world.py
+++ M11_world.py
@@ -2,9 +2,10 @@ import Commands
 
 def m11():
   return [
-    Commands.GyroDrive(speed=200, distance=550),
+    Commands.ActMotorTime(motor="left", time=2000, speed=500),
+    Commands.GyroDrive(speed=200, distance=475),
     Commands.Pivot(angle=-45, speed=150),
-    Commands.GyroDrive(distance=370, speed=200),
+    Commands.GyroDrive(distance=410, speed=200),
     Commands.ActMotorTime(motor="left", time=2000, speed=-500),
     Commands.DriveMM(distance=-1000, speed=250)
   ]
