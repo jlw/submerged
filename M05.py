54846b394aab13c607ad737cd7f48dfa88e5d9c8
Corwin Dennis <corwon52@gmail.com>
2024-11-30 15:05:51 -0600

    m05 update, up to linesquare

--- M05.py
+++ M05.py
@@ -2,20 +2,20 @@ import Commands
 
 def mo5():
   return [
-    # drive for krill & sample
-    Commands.GyroDrive(speed=150, distance=680),
-    # turn across back of board
-    Commands.Pivot(angle=-90),
-    Commands.GyroDrive(speed=150, distance=620),
-    # turn slightly to hit angler fish
-    # Commands.Pivot(angle=-5),
-    # Commands.GyroDrive(speed=150, distance=130),
-    Commands.Pivot(angle=15),
-    # turn back to avoid ship
-    Commands.GyroDrive(speed=150, distance=355),
-    Commands.Pivot(angle=-10),
-    Commands.GyroDrive(speed=150, distance=150),
+    Commands.GyroDrive(speed=150, distance=200),
     Commands.Pivot(angle=-35),
-    Commands.DriveMotor( motor="right", angle=90, speed=150),
-    Commands.GyroDrive(speed=150, distance=620)
+    Commands.GyroDrive(speed=150, distance=225),
+    Commands.GyroDrive(speed=150, distance=-50),
+    Commands.Pivot(angle=65),
+    Commands.GyroDrive(speed=175, distance=130),
+    Commands.Pivot(angle=-30),
+    Commands.GyroDrive(speed=150, distance=70),
+    Commands.Pivot(angle=-62),
+    Commands.GyroDrive(speed=150, distance=220),
+    Commands.Pivot(angle=-30),
+    
+    
+    #Commands.LineSquare(targetBlack=80, targetWhite=13, target=20, approachSpeed=100, finetuneSpeed=50, returnTime=2000)
+    
+    #Commands.GyroDrive(speed=150, distance=350),
   ]
