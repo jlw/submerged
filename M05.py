8f7684be84226aaf743c5642a27ddd93aecd19fa
Corwin Dennis <corwon52@gmail.com>
2024-12-04 17:30:10 -0600

    Comment m05

--- M05.py
+++ M05.py
@@ -2,27 +2,38 @@ import Commands
 
 def mo5():
   return [
+    # Lift the arm
     Commands.ActMotorTime(motor='right', speed=500, time=1750, wait=False),
     Commands.GyroDrive(speed=200, distance=320),
     Commands.Pivot(angle=-60),
+    # Ram the Quatropus
     Commands.GyroDrive(speed=200, distance=125),
     Commands.GyroDrive(speed=200, distance=-10),
     Commands.ActMotorTime(motor='right', speed=-500, time=1750, wait=False),
+    # Turn to coral and collect
     Commands.Pivot(angle=73),
     Commands.GyroDrive(speed=175, distance=120),
+    # Drive to sub station
     Commands.Pivot(angle=-27),
     Commands.GyroDrive(speed=200, distance=140),
     Commands.Pivot(angle=-64),
     Commands.GyroDrive(speed=200, distance=290),
+    # Turn to square
     Commands.Pivot(angle=30),
     Commands.GyroDrive(speed=150, distance=110),
-    #LineSquare
+    # LineSquare
     Commands.LineSquare(approachSpeed=100, finetuneSpeed=50, returnTime=4500),
+    # Turn to angler fish
     Commands.Pivot(angle=-70),
     Commands.GyroDrive(speed=200, distance=240),
+    # Scare it away
     Commands.Pivot(speed=200, angle=60),
     Commands.Pivot(speed=200, angle=-10),
     Commands.Pivot(speed=200, angle=60),
+    # Run to the coral
     Commands.GyroDrive(speed=200, distance=240),
-
+    # agressavly hit coral untill it revives it somehow
+    # Run away
+    # Scoop up all nearby wildlife
+    # Return to saftey
   ]
