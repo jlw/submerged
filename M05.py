2cdad0aeb8732944c27b7c05c7b916df3f3e2c30
Corwin Dennis <corwon52@gmail.com>
2024-12-04 17:10:26 -0600

    m05 up to anlger fish

--- M05.py
+++ M05.py
@@ -2,18 +2,27 @@ import Commands
 
 def mo5():
   return [
-    Commands.GyroDrive(speed=150, distance=200),
-    Commands.Pivot(angle=-35),
-    Commands.GyroDrive(speed=150, distance=225),
-    Commands.GyroDrive(speed=150, distance=-50),
-    Commands.Pivot(angle=65),
-    Commands.GyroDrive(speed=175, distance=130),
-    Commands.Pivot(angle=-30),
-    Commands.DriveMM(speed=175, distance=240, angle=-44),
-    Commands.DriveMM(speed=175, distance=250, angle=13),
-    #Commands.GyroDrive(speed=150, distance=50),
-    Commands.LineSquare(approachSpeed=100, finetuneSpeed=50, returnTime=5000),
-    Commands.DriveMM(speed=150, distance=-750, angle=-40),
-    #Commands.GyroDrive(speed=175, distance=150), 
-    
+    Commands.ActMotorTime(motor='right', speed=500, time=1750, wait=False),
+    Commands.GyroDrive(speed=200, distance=320),
+    Commands.Pivot(angle=-60),
+    Commands.GyroDrive(speed=200, distance=125),
+    Commands.GyroDrive(speed=200, distance=-10),
+    Commands.ActMotorTime(motor='right', speed=-500, time=1750, wait=False),
+    Commands.Pivot(angle=73),
+    Commands.GyroDrive(speed=175, distance=120),
+    Commands.Pivot(angle=-27),
+    Commands.GyroDrive(speed=200, distance=140),
+    Commands.Pivot(angle=-64),
+    Commands.GyroDrive(speed=200, distance=290),
+    Commands.Pivot(angle=30),
+    Commands.GyroDrive(speed=150, distance=110),
+    #LineSquare
+    Commands.LineSquare(approachSpeed=100, finetuneSpeed=50, returnTime=4500),
+    Commands.Pivot(angle=-70),
+    Commands.GyroDrive(speed=200, distance=240),
+    Commands.Pivot(speed=200, angle=60),
+    Commands.Pivot(speed=200, angle=-10),
+    Commands.Pivot(speed=200, angle=60),
+    Commands.GyroDrive(speed=200, distance=240),
+
   ]
